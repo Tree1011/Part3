6e816545affc3fa60925f1623045578f2f07d8c0
Tree <Nglinhchi1011@gmail.com>
2025-06-18 15:53:23 +0700

    Phonebook backend step2

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response)=>{
+    response.send(`Phone book has info for ${persons.length} people. <br><br>${Date()}`)
+})
+
 
 const PORT = 3001
 app.listen(PORT)
